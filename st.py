9fa950992b1796e594e90ba2733295d5e304d502
Jaisaran-N <83468853+Jaisaran-N@users.noreply.github.com>
2021-09-24 11:40:45 +0530

    Add files via upload

---
+++ st.py
@@ -0,0 +1,6 @@
+#Make A Directory
+#Create A Virtual Environment in it
+#install streamlit in the Virtual Environment(pip install streamlit)
+import streamlit as st
+st.title("Hello World")
+#run the file in command promt as streamlit/filename.py
